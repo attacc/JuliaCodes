752a08bab81d06f514e3e4b181fbc492b1b3014a
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2023-11-22 11:23:41 +0100

    Starting new code on Wannier guage

--- EOM_dipole/analysis.jl
+++ EOM_dipole/analysis.jl
@@ -24,10 +24,31 @@ function FFT_1D(times, freqs, pol, e_vec)
     dt=times[2]-times[1]
     for (itime,time) in enumerate(times)
       for (ifreqs, freq) in enumerate(freqs)
-          pol_w[ifreqs] =pol_w[ifreqs]+exp(1im*freq*time)*pol_edir[itime]
+          pol_w[ifreqs] =pol_w[ifreqs]+exp(-1im*freq*time)*pol_edir[itime]
       end
     end
     pol_w=pol_w*dt
+end
+
+function Divide_by_the_field(pol_w, times, itstart)
+  #
+  # I have to divide for the Fourier transform of the external field
+  #
+  # E(w) = \int dt E(t)  e^{ - omega * t }
+  #
+  # for a delta function
+  #
+  # E(w) = \int dt \delta(t-t_0} e^{-omega * t} = e^{ -omega t_0} 
+  #
+  tstart=itstart*(times[2]-times[1])
+  println("Starting time of delta function: ",tstart/fs2aut)
+  #
+  for (ifreqs, freq) in enumerate(freqs)
+     pol_w[ifreqs]=pol_w[ifreqs]*exp(1im * tstart*freq)
+  end
+  #
+  return pol_w
+  #
 end
 
 df = CSV.read("polarization.csv",DataFrame)
@@ -40,6 +61,8 @@ freqs_nsteps =200
 freqs=LinRange(freqs_range[1],freqs_range[2],freqs_nsteps)
 
 pol_w=FFT_1D(pol[:,1]*fs2aut, freqs, pol[:,2:3], e_vec)
+itstart=3 # should be the same of the EOM_dm_dipoles.jl
+pol_w=Divide_by_the_field(pol_w,pol[:,1]*fs2aut,itstart)
 #
 # Write data on file
 #
