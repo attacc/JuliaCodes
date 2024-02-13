a21c0009e83d608d048c5960854f484987f32c7f
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2024-02-13 22:46:44 +0100

    Input file separated from the code

---
+++ EOM_final5/EOM_linear.jl
@@ -0,0 +1,526 @@
+#
+# Density matrix EOM in the Wannier Gauge (TB approximation)
+# Claudio Attaccalite (2023)
+#
+using LinearAlgebra
+using Base.Threads
+
+
+include("EOM_input.jl")
+# 
+# EOM of the density matrix in Hamiltonian or Wannier gauge
+#
+k_grid=generate_unif_grid(n_k1, n_k2, lattice)
+
+# a generic off-diagonal matrix example (0 1; 1 0)
+off_diag=.~I(h_dim)
+
+TB_sol.h_dim=h_dim # Hamiltonian dimension
+TB_sol.eigenval=zeros(Float64,h_dim,k_grid.nk)
+TB_sol.eigenvec=zeros(Complex{Float64},h_dim,h_dim,k_grid.nk)
+
+println(" K-point list ")
+println(" nk = ",k_grid.nk)
+
+Eamp =sqrt(EInt*4.0*pi/SPEED_of_LIGHT)
+
+if dyn_props.dyn_gauge==H_gauge     
+	println("* * * Hamiltonian gauge * * * ")             
+else 
+	println("* * * Wannier gauge * * * ") 
+end
+if dyn_props.use_dipoles 
+	println("* * * Dipole approximation * * * ") 
+else 
+   println("* * * Full coupling with r = id/dk + A_w * * * ") 
+end
+
+if dyn_props.use_UdU_for_dipoles
+   println("* * * Using UdU to build dipoles * * * ") 
+else
+   println("* * * Using dH/dk to build dipoles * * * ") 
+end
+if dyn_props.include_drho_dk
+   println("* * * Using drho/dk in the dynamics * * * ") 
+end
+
+println(" Field name : ",field_name)
+println(" Number of threads: ",Threads.nthreads())
+
+println("Tight-binding gauge : $TB_gauge ")
+println("Delta-k for derivatives : $dk ")
+
+println("Orbitals coordinates : ")
+println(orbitals.tau[1])
+println(orbitals.tau[2])
+
+println("Building Hamiltonian: ")
+H_h=zeros(Complex{Float64},h_dim,h_dim,k_grid.nk)
+TB_sol.H_w=zeros(Complex{Float64},h_dim,h_dim,k_grid.nk)
+
+Threads.@threads for ik in ProgressBar(1:k_grid.nk)
+    H_w=Hamiltonian(k_grid.kpt[:,ik],TB_gauge)
+    data= eigen(H_w)      # Diagonalize the matrix
+    TB_sol.eigenval[:,ik]   = data.values
+    TB_sol.eigenvec[:,:,ik] = data.vectors
+    for i in 1:h_dim
+      H_h[i,i,ik]=TB_sol.eigenval[i,ik]
+    end
+    TB_sol.H_w[:,:,ik]=H_w
+    #
+    # Fix phase of eigenvectors
+    #
+    TB_sol.eigenvec[:,:,ik]=fix_eigenvec_phase(TB_sol.eigenvec[:,:,ik])
+    #
+end
+
+#Hamiltonian info
+dir_gap=minimum(TB_sol.eigenval[2,:]-TB_sol.eigenval[1,:])
+println("Direct gap : ",dir_gap*ha2ev," [eV] ")
+ind_gap=minimum(TB_sol.eigenval[2,:])-maximum(TB_sol.eigenval[1,:])
+println("Indirect gap : ",ind_gap*ha2ev," [eV] ")
+
+# k-gradients of Hmailtonian, eigenvalues and eigenvectors
+Dip_h=zeros(Complex{Float64},h_dim,h_dim,s_dim,k_grid.nk)
+∇H_w =zeros(Complex{Float64},h_dim,h_dim,s_dim,k_grid.nk)
+
+println("Building ∇H and Dipoles: ")
+Threads.@threads for ik in ProgressBar(1:k_grid.nk)
+# Dipoles 
+  #Gradient of the Hamiltonian by finite difference 
+  ∇H_w[:,:,:,ik]=Grad_H(ik, k_grid, lattice, Hamiltonian, TB_sol, TB_gauge,dk)
+  #
+  # Build dipoles
+  for id in 1:s_dim
+        Dip_h[:,:,id,ik]=WH_rotate(∇H_w[:,:,id,ik],TB_sol.eigenvec[:,:,ik])
+# I set to zero the diagonal part of dipoles
+	Dip_h[:,:,id,ik]=Dip_h[:,:,id,ik].*off_diag
+  end
+
+# Now I have to divide for the energies
+#
+#  p = \grad_k H 
+#
+#  r_{ij} = i * p_{ij}/(e_j - e_i)
+#
+#  (diagonal terms are set to zero)
+#
+  for i in 1:h_dim
+      for j in i+1:h_dim
+          Dip_h[i,j,:,ik]= 1im*Dip_h[i,j,:,ik]/(TB_sol.eigenval[j,ik]-TB_sol.eigenval[i,ik])
+          Dip_h[j,i,:,ik]=conj(Dip_h[i,j,:,ik])
+      end
+  end
+  #
+  # I bring them back in Wannier gauge
+  #
+  if dyn_props.dyn_gauge==W_gauge
+    for id in 1:s_dim
+        Dip_h[:,:,id,ik]=HW_rotate(Dip_h[:,:,id,ik],TB_sol.eigenvec[:,:,ik])
+     end
+  end
+end
+#
+# Calculate Overlaps in the Wannier Gauge
+#
+# Calculate A^H(k) 
+#
+println("Calculate A^H(k) : ")
+A_h=zeros(Complex{Float64},h_dim,h_dim,s_dim,k_grid.nk)
+A_w=zeros(Complex{Float64},h_dim,h_dim,s_dim,k_grid.nk)
+
+if dyn_props.include_A_w
+  println("Calculate A_w: ")
+  Threads.@threads for ik in ProgressBar(1:k_grid.nk)
+  #
+  #
+  # Calculate A(W) and rotate in H-gauge
+  # Eq. II.13 of https://arxiv.org/pdf/1904.00283.pdf 
+  # Notice that in TB-approxamation the Berry Connection is independent from k
+  A_w[:,:,:,ik]=Berry_Connection(k_grid)
+  #
+  # Then I rotate from W -> H
+  #
+  for id in 1:s_dim
+     A_h[:,:,id,ik]=WH_rotate(A_w[:,:,id,ik],TB_sol.eigenvec[:,:,ik])
+  end
+  #
+  end
+end
+
+
+println("Calculate UdU:")
+Threads.@threads for ik in ProgressBar(1:k_grid.nk)
+  #  
+  # Calculate U^+ \d/dk U
+  #
+  #Gradient of eigenvectors/eigenvalus on the regular grid
+  #
+  UdU=Grad_U(ik, k_grid, lattice, TB_sol, TB_gauge,dk)
+  #
+  #  Using the fixing of the guage
+  #
+  U=TB_sol.eigenvec[:,:,ik]
+  for id in 1:s_dim
+    UdU[:,:,id]=(U')*UdU[:,:,id]
+  end
+  #
+  A_h[:,:,:,ik]+=1im*UdU[:,:,:]
+  #
+  if dyn_props.use_UdU_for_dipoles
+    Dip_h[:,:,:,ik]=1im*UdU[:,:,:]
+    if dyn_props.dyn_gauge==W_gauge
+      for id in 1:s_dim
+          Dip_h[:,:,id,ik]=HW_rotate(Dip_h[:,:,id,ik],TB_sol.eigenvec[:,:,ik])
+       end
+    end
+  end
+  #
+end
+#
+# Input paramters for linear optics with delta function
+#
+t_range = 0.0:dt:t_end
+n_steps=size(t_range)[1]
+#
+# Buildo rho_0
+#
+rho0=zeros(Complex{Float64},h_dim,h_dim,k_grid.nk)
+rho0[1,1,:].=1.0
+#
+# Transform in Wannier Gauge
+#
+if dyn_props.dyn_gauge==W_gauge
+  Threads.@threads for ik in 1:k_grid.nk
+     rho0[:,:,ik]=HW_rotate(rho0[:,:,ik],TB_sol.eigenvec[:,:,ik])
+  end
+end
+#
+#
+#
+println(" * * * Linear response from density matrix EOM within dipole approx. * * *")
+println("Time rage ",0.0," - ",t_end/fs2aut)
+println("Number of steps ",n_steps)
+if dyn_props.damping
+   println("Dephasing time ",T_2/fs2aut," [fs] ")
+end
+println("External field versor :",E_vec)
+
+#
+itstart = 20 # start of the external field
+
+function get_Efield(t, ftype; itstart=3)
+	#
+	# Field in direction y
+	#
+	if ftype=="delta"
+  	  #
+          if t>=(itstart-1)*dt && t<itstart*dt 
+  		a_t=1.0/dt
+  	  else
+		a_t=0.0
+	  end
+          #
+          a_t=a_t*Eamp
+	  #
+ 	elseif ftype=="PHHG"
+	  w    =0.4132/ha2ev # Parameter from De Silva
+	  sigma=34.2*fs2aut  # Parameter from De Silva
+	  T_0  = itstart*dt
+	  if (t-T_0)>=sigma || (t-T_0)<0.0
+	          a_t=0.0
+	  else
+                 a_t =Eamp*(sin(pi*(t-T_0)/sigma))^2*sin(w*(t-T_0))
+	  end
+	else
+	  println("Field unknown!! ")	
+	  exit()
+	end
+	#
+	Efield=a_t*E_vec
+	#
+	return Efield
+end
+
+# 
+# Different possible matrix for the coupling
+# with the external field in the commutator
+#
+# Dip_h= simple dipoles (ok for linear response)  dH/dk / (e_i -e_j)
+# A_h  = Eq. II.13 of arXiv:1904.00283v2  (Berry connection in Hamiltonian gauge)
+# A_w  = Eq. II.9 with approximation III.1 of of arXiv:1904.00283v2 (Berry connection in Wannier gauge)
+#
+if dyn_props.use_dipoles
+  DIP_matrix=Dip_h
+else
+  if dyn_props.dyn_gauge==H_gauge
+    DIP_maitrx=A_h
+  elseif dyn_props.dyn_gauge==W_gauge
+    DIP_matrix=A_w
+  end
+end
+
+
+function deriv_rho(rho, t)
+	#
+        # Variable of the dynamics
+        #
+	h_dim=2
+	#
+	d_rho=zeros(Complex{Float64},h_dim,h_dim,k_grid.nk) #
+	#
+	# Hamiltonian term
+	#
+        if dyn_props.dyn_gauge==H_gauge
+          # in h-space the Hamiltonian is diagonal
+	  Threads.@threads for ik in 1:k_grid.nk
+             for ib in 1:h_dim, ic in 1:h_dim
+  	       d_rho[ib,ic,ik] =TB_sol.eigenval[ib,ik]*rho[ib,ic,ik]-rho[ib,ic,ik]*TB_sol.eigenval[ic,ik]
+             end
+	  end
+        else
+          # in the w-space the Hamiltonian is not diagonal
+	  Threads.@threads for ik in 1:k_grid.nk
+ 	     d_rho[:,:,ik]=TB_sol.H_w[:,:,ik]*rho[:,:,ik]-rho[:,:,ik]*TB_sol.H_w[:,:,ik]
+	  end
+        end
+	#
+	# Electrinc field
+	#
+	E_field=get_Efield(t, field_name,itstart=itstart)
+	#
+	Threads.@threads for ik in 1:k_grid.nk
+	  #
+	  # Dipole term for the commutator dot field
+	  #
+          E_dot_DIP=zeros(Complex{Float64},h_dim,h_dim)
+          for id in 1:s_dim
+            E_dot_DIP+=E_field[id]*DIP_matrix[:,:,id,ik]
+          end
+          #
+          # Commutator D*rho-rho*D
+          # 
+	  d_rho[:,:,ik]=d_rho[:,:,ik]+(E_dot_DIP[:,:]*rho[:,:,ik]-rho[:,:,ik]*E_dot_DIP[:,:])
+          #
+	end
+	#
+        if !dyn_props.use_dipoles &&  dyn_props.include_drho_dk
+          # 
+          # Add d_rho/dk
+          #
+   	  Threads.@threads for ik in 1:k_grid.nk
+            Dk_rho=Evaluate_Dk_rho(rho, ik, k_grid, TB_sol.eigenvec, lattice)
+            for id in 1:s_dim
+              d_rho[:,:,ik]+=1im*E_field[id]*Dk_rho[:,:,id]
+            end
+          end
+          #
+        end
+        #
+	# Damping
+	#
+	if T_2!=0.0 && dyn_props.damping==true
+           #
+	   if dyn_props.dyn_gauge==H_gauge
+	     Threads.@threads for ik in 1:k_grid.nk
+               Δrho=rho[:,:,ik]-rho0[:,:,ik]
+	       d_rho[:,:,ik]=d_rho[:,:,ik]-1im/T_2*off_diag.*Δrho
+             end
+       	   else
+	     Threads.@threads for ik in 1:k_grid.nk
+               Δrho=rho[:,:,ik]-rho0[:,:,ik]
+	       Δrho=off_diag.*WH_rotate(Δrho,TB_sol.eigenvec[:,:,ik])
+	       damp_mat=HW_rotate(Δrho,TB_sol.eigenvec[:,:,ik])
+	       d_rho[:,:,ik]=d_rho[:,:,ik]-1im/T_2*damp_mat
+	     end
+	   end
+	end
+        #
+	Threads.@threads for ik in 1:k_grid.nk
+	  d_rho[:,:,ik].=-1im*d_rho[:,:,ik]
+	end
+	#
+    return d_rho
+end
+
+function get_polarization(rho_s)
+    nsteps=size(rho_s,1)
+    pol=zeros(Float64,nsteps,s_dim)
+    println("Calculate polarization: ")
+    Threads.@threads for it in ProgressBar(1:nsteps)
+      for ik in 1:k_grid.nk
+         if dyn_props.dyn_gauge==W_gauge
+           rho=WH_rotate(rho_s[it,:,:,ik],TB_sol.eigenvec[:,:,ik])
+         else
+           rho=rho_s[it,:,:,ik]
+         end
+         for id in 1:s_dim
+            if dyn_props.dyn_gauge==W_gauge
+               dip=WH_rotate(Dip_h[:,:,id,ik],TB_sol.eigenvec[:,:,ik]).*off_diag
+             else
+               dip=Dip_h[:,:,id,ik].*off_diag
+             end
+       	     pol[it,id]=pol[it,id]+real.(sum(dip.*transpose(rho)))
+	   end
+        end
+    end
+    pol=pol/k_grid.nk
+    return pol
+end 
+
+function get_current(rho_s)
+    nsteps=size(rho_s,1)
+    j_intra=zeros(Float64,nsteps,s_dim)
+    j_inter=zeros(Float64,nsteps,s_dim)
+    println("Calculate current: ")
+    for it in ProgressBar(1:nsteps)
+       j_intra[it,:],j_inter[it,:]=current(rho_s[it,:,:,:])
+    end
+    return j_intra,j_inter
+end
+
+function current(rho)
+ j_intra_t=zeros(Float64, s_dim, Threads.nthreads())
+ j_inter_t=zeros(Float64, s_dim, Threads.nthreads())
+ if dyn_props.dyn_gauge==H_gauge
+   Threads.@threads for ik in 1:k_grid.nk
+     rho_t_H=transpose(rho[:,:,ik])
+     t_id=Threads.threadid()
+     for id in 1:s_dim
+        ∇H_h=WH_rotate(∇H_w[:,:,id,ik],TB_sol.eigenvec[:,:,ik]) #.*off_diag
+        j_intra_t[id,t_id]=j_intra_t[id,t_id]-real.(sum(∇H_h.*rho_t_H))
+        commutator=A_h[:,:,id,ik]*H_h[:,:,ik]-H_h[:,:,ik]*A_h[:,:,id,ik]
+        j_inter_t[id,t_id]=j_inter_t[id,t_id]-imag(sum(commutator.*rho_t_H))
+     end
+   end
+ else
+   if props.curr_gauge==H_gauge
+#  Current using the Hamiltonian gauge
+     Threads.@threads for ik in 1:k_grid.nk
+        rho_h=WH_rotate(rho[:,:,ik],TB_sol.eigenvec[:,:,ik])
+        rho_h=transpose(rho_h)
+        t_id=Threads.threadid()
+        for id in 1:s_dim
+           # 
+           # Not sure if I should multiply for off_diag this is not clear
+           # If I do not do so I get finite current after the pulse (that it is fine)
+           # Probably introducing a LifeTime for the electron remove the necessity
+           # of this off_diag or a windows for the current 
+           #
+           ∇H_h=WH_rotate(∇H_w[:,:,id,ik],TB_sol.eigenvec[:,:,ik]) #.*off_diag
+           j_intra_t[id,t_id]=j_intra_t[id,t_id]-real.(sum(∇H_h.*rho_h))
+           commutator=A_h[:,:,id,ik]*H_h[:,:,ik]-H_h[:,:,ik]*A_h[:,:,id,ik]
+	   j_inter_t[id,t_id]=j_inter_t[id,t_id]-imag(sum(commutator.*rho_h))
+	end
+     end
+
+   elseif props.curr_gauge==W_gauge
+#    Current using the Wannier gauge
+     Threads.@threads for ik in 1:k_grid.nk
+       t_id=Threads.threadid()
+       rho_w=transpose(rho[:,:,ik])
+       for id in 1:s_dim
+          j_intra_t[id,t_id]=j_intra_t[id,t_id]-real.(sum(∇H_w[:,:,id,ik].*rho_w))
+          commutator=A_w[:,:,id,ik]*TB_sol.H_w[:,:,ik]-TB_sol.H_w[:,:,ik]*A_w[:,:,id,ik]
+	  j_inter_t[id,t_id]=j_inter_t[id,t_id]-imag(sum(commutator.*rho_w))
+       end
+     end
+   end
+  end
+  j_intra=zeros(Float64,s_dim)
+  j_inter=zeros(Float64,s_dim)
+  for id in 1:s_dim
+      j_intra[id]=sum(j_intra_t[id,:])/k_grid.nk
+      j_inter[id]=sum(j_inter_t[id,:])/k_grid.nk
+  end
+  return j_intra,j_inter
+end 
+
+# Solve EOM
+solution = rungekutta2_dm(deriv_rho, rho0, t_range)
+#solution = rungekutta4_dm(deriv_rho, rho0, t_range)
+
+if props.eval_pol
+  # Calculate the polarization in time and frequency
+  pol=get_polarization(solution)
+end
+if props.eval_curr
+  j0_intra,j0_inter=current(rho0)
+  j_intra,j_inter  =get_current(solution)
+  Threads.@threads for it in 1:n_steps
+    j_intra[it,:]-= j0_intra[:]
+    j_inter[it,:]-= j0_inter[:]
+  end
+end
+
+# Generate headers
+
+function generate_header(k_grid=nothing,dyn_props=nothing,props=nothing)
+   header="#\n# * * * EOM of the density matrix * * * \n#\n#"
+
+   if k_grid != nothing
+     header*="# k-point grid: $(k_grid.nk_dir[1]) - $(k_grid.nk_dir[2]) \n"
+   end
+   if dyn_props != nothing
+     if dyn_props.dyn_gauge==H_gauge
+         header*="# structure gauge : Hamiltonian \n"
+     else
+         header*="# structure gauge : Wannier \n"
+     end
+     header*="# include drho/dk in the dynamics: $(dyn_props.include_drho_dk) \n"
+     header*="# include A_w in the dynamics: $(dyn_props.include_A_w) \n"
+     header*="# use dipoles : $(dyn_props.use_dipoles) \n"
+     header*="# use damping : $(dyn_props.damping) \n"
+     header*="# use UdU for dipoles : $(dyn_props.use_UdU_for_dipoles) \n"
+   end
+
+   if props != nothing
+     header*="# calculate polarization : $(props.eval_pol)\n"
+     header*="# calculate current      : $(props.eval_curr)\n"
+     header*="# current gauge          : $(props.curr_gauge) \n"
+     header*="# print dm               : $(props.print_dm)\n"
+   end
+   header*="#\n#\n"
+  return header
+end
+
+
+# Write polarization and external field on disk
+
+t_and_E=zeros(Float64,n_steps,3)
+Threads.@threads for it in 1:n_steps
+ t=it*dt
+ E_field_t=get_Efield(t,field_name,itstart=itstart)
+ t_and_E[it,:]=[t/fs2aut,E_field_t[1],E_field_t[2]]
+end
+
+header=generate_header(k_grid,dyn_props,props)
+
+if props.eval_pol
+  f = open("polarization.csv","w") 
+  write(f,header)
+  write(f,"#time[fs] polarization_x  polarization_y\n") 
+  for it in 1:n_steps
+    write(f,"$(t_and_E[it,1]), $(pol[it,1]),  $(pol[it,2]) \n")
+  end
+  close(f)
+end
+
+
+if props.eval_curr
+  f = open("current.csv","w") 
+  write(f,header)
+  write(f,"#time[fs] j_intra_x  j_intra_y  j_inter_x  j_inter_y\n") 
+  for it in 1:n_steps
+      write(f,"$(t_and_E[it,1]), $(j_intra[it,1]),  $(j_intra[it,2]),  $(j_inter[it,1]),  $(j_inter[it,2]) \n")
+  end
+  close(f)
+end
+
+f = open("external_field.csv","w") 
+write(f,header)
+write(f,"#time[fs]  efield_x efield_y\n") 
+for it in 1:n_steps
+  write(f,"$(t_and_E[it,1]),  $(t_and_E[it,2]),  $(t_and_E[it,3]) \n")
+end
+
---
+++ EOM_final5/bz_sampling.jl
@@ -0,0 +1,103 @@
+module BZ_sampling
+
+using Printf
+
+export generate_circuit,generate_unif_grid,get_k_neighbor,print_k_grid
+
+mutable struct K_Grid
+	kpt::Array{Float64,2}
+	nk_dir::Array{Int,1}
+        nk::Int
+	ik_map::Array{Int,3}
+	ik_map_inv::Array{Int,2}
+end
+
+function generate_circuit(points, n_steps)
+	println("Generate k-path ")
+	n_points=length(points)
+	@printf("number of points:%5i \n",n_points)
+	if n_points <= 1
+		error("number of points less or equal to 1 ")
+	end
+	for i in 1:n_points
+		@printf("v(%d) = %s \n",i,points[i])
+	end
+	path=Any[]
+	for i in 1:(n_points-1)
+		for j in 0:(n_steps-1)	
+			dp=(points[i+1]-points[i])/n_steps
+			push!(path,points[i]+dp*j)
+		end
+	end
+	push!(path,points[n_points])
+	return path
+ end
+ #
+ function generate_unif_grid(n_kx, n_ky, lattice)
+     scale=1.0
+     nk   =n_kx*n_ky
+     kpt  =zeros(Float64,lattice.dim,nk)
+     ik_grid    =zeros(Int,n_kx,n_ky,1)
+     ik_grid_inv=zeros(Int,lattice.dim,nk)
+
+     vec_crystal=zeros(Float64,lattice.dim)
+     dx=scale/n_kx
+     dy=scale/n_ky
+
+     ik=1
+     for ix in 0:(n_kx-1),iy in 0:(n_ky-1)
+           ik_grid[ix+1,iy+1,1]=ik
+           ik_grid_inv[:,ik]   =[ix+1,iy+1]
+	   vec_crystal[1]=dx*ix
+	   vec_crystal[2]=dy*iy
+	   kpt[:,ik]=lattice.rvectors[1]*vec_crystal[1]+lattice.rvectors[2]*vec_crystal[2]
+	   ik=ik+1
+     end
+
+     k_grid = K_Grid(
+		kpt,
+		[n_kx,n_ky,1],
+                nk,
+		ik_grid,
+		ik_grid_inv
+	       )
+     return k_grid
+ end
+ #
+ function print_k_grid(k_grid, lattice)
+    println("K-points grid ")
+    println("grid dimensions : ",k_grid.nk_dir, "\n\n")
+    for ik in 1:size(k_grid.kpt, 2)
+       k_crystal=lattice.b_mat_inv*k_grid.kpt[:,ik]
+       println("ik ",ik," kpt ",k_crystal," ik_grid ",k_grid.ik_map_inv[:,ik])
+       ik_left =get_k_neighbor(ik,1,1,k_grid)
+       ik_right=get_k_neighbor(ik,1,-1,k_grid)
+       println("x-neighboar ",ik_left," - ",ik_right)
+    end
+ end
+ #
+ function get_k_neighbor(ik,id,istep,k_grid)
+     s_dim=2
+
+     # position of k-point in the integer grid
+     ik_n  =k_grid.ik_map_inv[:,ik]
+
+     # shift the k-points by an integer step
+     ik_n[id] =ik_n[id]+istep 
+#
+     d_size=k_grid.nk_dir[id]
+     if ik_n[id] > d_size
+        ik_n[id]=ik_n[id]-d_size
+     elseif ik_n[id]<=0
+        ik_n[id]=ik_n[id]+d_size
+     end
+#
+     ik_indx=[1,1,1]
+     for id in 1:s_dim
+        ik_indx[id]=ik_n[id]
+     end
+     ik_n=k_grid.ik_map[ik_indx[1],ik_indx[2],ik_indx[3]]
+     return ik_n
+ end
+ #
+end
